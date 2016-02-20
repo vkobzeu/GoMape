569fc0a023ed14ff80e1662aa3e27c5b3520ccc6
vitali kobzeu <kobzev.vitaly@gmail.com>
2016-02-20 14:49:41 -0500

    implemented connectivity and set bg color

--- HarmanBluetoothClient/HarmanBluetoothClient/PulseHandlerInterfaceImpl.cs
+++ HarmanBluetoothClient/HarmanBluetoothClient/PulseHandlerInterfaceImpl.cs
@@ -5,17 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Harman.Pulse.Stubs;
+using InTheHand.Net.Sockets;
+using InTheHand.Net.Bluetooth;
 
 namespace HarmanBluetoothClient
 {
     public class PulseHandlerInterfaceImpl : PulseHandlerInterface
     {
+        const string device_name = "JBL Pulse 2";
+
+        private BluetoothClient _bluetoothClient;
+
         public bool? IsConnectMasterDevice
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
+            get; set;
         }
 
         public bool? CaptureColorFromColorPicker()
@@ -25,7 +28,43 @@ namespace HarmanBluetoothClient
 
         public bool? ConnectMasterDevice(Activity paramActivity)
         {
-            throw new NotImplementedException();
+            _bluetoothClient = new BluetoothClient();
+
+            IEnumerable<BluetoothDeviceInfo> targetDevices = null;
+
+            Console.WriteLine("Discovering...");
+            int att = 5;
+            while (att > 0)
+            {
+                att--;
+                var devices = _bluetoothClient.DiscoverDevices();
+
+                targetDevices = devices.Where(d => d.DeviceName == device_name);
+
+                if (!targetDevices.Any())
+                {
+                    Console.WriteLine("No device by name JBL Pulse 2 found! Retrying...");
+                }
+                else break;
+            }
+            if (targetDevices.Count() > 1)
+            {
+                Console.WriteLine("More than one Pulse 2 found! Picking one...");
+            }
+            BluetoothDeviceInfo pulse2 = targetDevices.First();
+
+            Console.WriteLine(String.Format("Pulse 2 discovered, address is: {0}", pulse2.DeviceAddress));
+            var targetAddress = pulse2.DeviceAddress;
+
+            //new Guid("00001101-0000-1000-8000-00805F9B34FB")));
+            _bluetoothClient.Connect(targetAddress, BluetoothService.SerialPort);
+
+            if (_bluetoothClient.Connected)
+            {
+                Console.WriteLine("Connected to device's Serial Port");
+                IsConnectMasterDevice = true;
+            }
+            return true;
         }
 
         public bool? GetBrightness()
@@ -60,7 +99,12 @@ namespace HarmanBluetoothClient
 
         public bool? SetBackgroundColor(PulseColor paramPulseColor, bool paramBoolean)
         {
-            throw new NotImplementedException();
+            int colorIdx = WebColorHelper.RGBToWeb216Index(paramPulseColor);
+
+            sbyte[] cmd = new sbyte[] { -86, 88, 2, (sbyte)colorIdx, (sbyte)(0) };
+            _bluetoothClient.Client.Send(cmd.Select(b => (byte)b).ToArray());
+
+            return true;
         }
 
         public bool? SetBrightness(int paramInt)
