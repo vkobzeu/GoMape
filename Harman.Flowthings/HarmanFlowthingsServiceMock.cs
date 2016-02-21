31e16811959bb3eb34b4bea00bd8ab103cb52490
Dmitry Komin <dkomin85@gmail.com>
2016-02-21 10:10:34 -0500

    111

---
+++ Harman.Flowthings/HarmanFlowthingsServiceMock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harman.Flowthings
+{
+    public class HarmanFlowthingsServiceMock : IHarmanFlowthingsService
+    {
+        public float GetSensorLuminance(string sensorId)
+        {
+            return 500;
+        }
+    }
+}
